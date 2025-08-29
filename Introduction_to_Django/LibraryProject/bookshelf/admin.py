6a2266ec0cd4f42852bdd5fe2867eadaabfec9c8
Daniel kyeremateng Amartey <danielkyerematengamartey@gmail.com>
2025-08-29 19:36:55 +0000

    Update admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+class BookAdmin('title', 'author', 'Publication_year')
+list_display = ('author', 'publication_year')
+list_filter = ('author', 'publication_year')
+search_fields = ('title', 'author')
+
+admin.site.register(Book, BookAdmin)
+
