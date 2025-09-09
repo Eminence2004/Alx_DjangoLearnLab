5529e1dc9466b0c6e2d8fb2d1a96fb017632cacd
Daniel kyeremateng Amartey <danielkyerematengamartey@gmail.com>
2025-09-09 09:34:16 +0000

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -12,9 +12,10 @@ class Author(models.Model):
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
-
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+    
     def __str__(self):
         return self.title
 
@@ -38,6 +39,12 @@ class UserProfile(models.Model):
         ("Admin", "Admin"),
         ("Librarian", "Librarian"),
         ("Member", "Member"),
+
+         class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
     ]
 
  user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -57,4 +64,5 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
         
+
 
