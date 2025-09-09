a547eb9be6103aed7c0e664c88538e1de7809a9d
Daniel kyeremateng Amartey <danielkyerematengamartey@gmail.com>
2025-09-09 10:52:36 +0000

    Update models.py

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -64,5 +64,6 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
         
+
 
 
