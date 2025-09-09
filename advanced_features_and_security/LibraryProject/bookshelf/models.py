1eab26e3a01e76d7c2490c3d6ebfd07d481e43f7
Daniel kyeremateng Amartey <danielkyerematengamartey@gmail.com>
2025-09-09 11:08:38 +0000

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -39,6 +39,36 @@ class CustomUser(AbstractUser):
     profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
 
     objects = CustomUserManager()
-
+    
     def __str__(self):
         return self.username
+
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
+class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
