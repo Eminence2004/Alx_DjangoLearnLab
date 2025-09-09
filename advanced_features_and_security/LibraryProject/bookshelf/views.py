b435173cc691d13195162126e9b87db73eb087bf
Daniel kyeremateng Amartey <danielkyerematengamartey@gmail.com>
2025-09-09 11:29:35 +0000

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,12 +2,26 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 
 # Create your views here.
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, "bookshelf/book_list.html", {"books": books})
+
+def form_example_view(request):
+    results = None
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data['search_query']
+            # Safe ORM query, prevents SQL injection
+            results = Book.objects.filter(title__icontains=query)
+    else:
+        form = ExampleForm()
+
+    return render(request, "bookshelf/form_example.html", {"form": form, "results": results})
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def add_book(request):
@@ -22,4 +36,5 @@ def edit_book(request, book_id):
 def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     return HttpResponse(f"Deleting book: {book.title} (requires can_delete permission).")
+
 
