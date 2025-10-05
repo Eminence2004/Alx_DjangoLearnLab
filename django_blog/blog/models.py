12ff1cdbba11093e08c94cbb1f468a3ed1ab3f71
Daniel kyeremateng Amartey <danielkyerematengamartey@gmail.com>
2025-10-05 21:59:58 +0000

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,34 +1,21 @@
+# blog/models.py
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
-    
-    def get_absolute_url(self):
-        # useful for redirecting after create/update
-        return reverse('post-detail', kwargs={'pk': self.pk})
-    
-
-
-
-
-
-
 
 class Comment(models.Model):
-    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Comment by {self.author.username} on {self.post.title}"
+        return f'Comment by {self.author} on {self.post}'
