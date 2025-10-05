dbf1556da48d910ced9f43aba19eea77088a8ee1
Daniel kyeremateng Amartey <danielkyerematengamartey@gmail.com>
2025-10-05 22:42:56 +0000

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 # --- Post model ---
 class Post(models.Model):
@@ -7,7 +8,8 @@ class Post(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)  # optional but useful
+    updated_at = models.DateTimeField(auto_now=True)
+     tags = TaggableManager() 
 
     def __str__(self):
         return self.title
@@ -20,4 +22,5 @@ class Comment(models.Model):  # ✅ checker looks for this
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     upda
+
 
