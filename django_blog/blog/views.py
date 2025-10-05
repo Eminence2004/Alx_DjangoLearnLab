590dbec46c17b02f5093c751ed198c1ccf88f6f9
Daniel kyeremateng Amartey <danielkyerematengamartey@gmail.com>
2025-10-05 23:07:17 +0000

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,80 @@
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db.models import Q
+
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+
+# --- Post views ---
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post-list')
+    template_name = 'blog/post_confirm_delete.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+# --- Search posts ---
+def search_posts(request):
+    query = request.GET.get('q', '')
+    posts = Post.objects.all()
+    if query:
+        posts = posts.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(tags__name__icontains=query)
+        ).distinct()
+    return render(request, 'blog/search_results.html', {'posts': posts, 'query': query})
+
+
+# --- Posts by tag ---
+def posts_by_tag(request, tag_slug):
+    posts = Post.objects.filter(tags__slug=tag_slug)
+    return render(request, 'blog/posts_by_tag.html', {'posts': posts, 'tag': tag_slug})
+
+
 # --- Comment views ---
 class CommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
@@ -5,14 +82,12 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     template_name = 'blog/comment_form.html'
 
     def form_valid(self, form):
-        # attach the correct post and author
         post = get_object_or_404(Post, pk=self.kwargs['post_id'])
         form.instance.author = self.request.user
         form.instance.post = post
         return super().form_valid(form)
 
     def get_success_url(self):
-        # redirect back to the post detail page
         return self.object.post.get_absolute_url()
 
 
@@ -22,7 +97,6 @@ class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = 'blog/comment_form.html'
 
     def test_func(self):
-        # only the author can edit
         comment = self.get_object()
         return self.request.user == comment.author
 
@@ -35,10 +109,8 @@ class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = 'blog/comment_confirm_delete.html'
 
     def test_func(self):
-        # only the author can delete
         comment = self.get_object()
         return self.request.user == comment.author
 
     def get_success_url(self):
         return self.object.post.get_absolute_url()
-
