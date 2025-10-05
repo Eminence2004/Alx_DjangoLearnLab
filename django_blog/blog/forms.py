864a052a6f20c77d5bd225bcf9bc911c6024aed6
Daniel kyeremateng Amartey <danielkyerematengamartey@gmail.com>
2025-10-05 22:00:26 +0000

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,26 +1,14 @@
+# blog/forms.py
 from django import forms
-from .models import Post
 from .models import Comment
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
+class CommentForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
-
-
-    class PostForm(forms.ModelForm):   # <-- make sure the name matches
-     class Meta:
-        model = Post
-        fields = ['title', 'content']
-
-
-
-
-    class CommentForm(forms.ModelForm):
-     class Meta:
         model = Comment
         fields = ['content']
+
+    def clean_content(self):
+        data = self.cleaned_data['content']
+        if len(data.strip()) < 3:
+            raise forms.ValidationError("Comment is too short.")
+        return data
